4347977eb525f15569e1119b3eaaaf15d3345618
Gilson Silva <gilsonsilvati@gmail.com>
2020-05-16 22:27:50 -0300

    Tratamento de exceções parte 1

--- src/test/java/br/com/teste/domain/service/LocacaoServiceTest.java
+++ src/test/java/br/com/teste/domain/service/LocacaoServiceTest.java
@@ -4,12 +4,15 @@ import static br.com.teste.domain.util.DataUtil.isMesmaData;
 import static br.com.teste.domain.util.DataUtil.obterDataComDiferencaDias;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.com.teste.domain.model.Filme;
 import br.com.teste.domain.model.Locacao;
@@ -20,8 +23,12 @@ public class LocacaoServiceTest {
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 	
+	@SuppressWarnings("deprecation")
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
-	public void teste() {
+	public void teste() throws Exception {
 		// cenario
 		LocacaoService locacaoService = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
@@ -31,9 +38,53 @@ public class LocacaoServiceTest {
 		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 		
 		// verificacao
-		error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
 		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(false));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+	}
+	
+	// 1° Forma: elegante
+	@Test(expected = Exception.class)
+	public void testLocacao_filmeSemEstoque_1() throws Exception {
+		// cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+
+		// acao
+		locacaoService.alugarFilme(usuario, filme);
+	}
+	
+	// 2° Forma: robusta
+	@Test
+	public void testLocacao_filmeSemEstoque_2() {
+		// cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+
+		// acao
+		try {
+			locacaoService.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lançado uma exceção");
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is(equalTo("Filme sem estoque")));
+		}
+	}
+	
+	// 3° Forma: nova
+	@Test
+	public void testLocacao_filmeSemEstoque_3() throws Exception {
+		// cenario
+		LocacaoService locacaoService = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		// acao
+		locacaoService.alugarFilme(usuario, filme);
 	}
 
 }
